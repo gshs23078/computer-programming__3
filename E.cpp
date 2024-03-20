dd77315744b9b88a5da2f82550a0c81241cce0e7
gshs23078 <129289574+gshs23078@users.noreply.github.com>
2024-03-20 20:55:57 +0900

    Update E.cpp

--- E.cpp
+++ E.cpp
@@ -1,15 +1,19 @@
 #include <iostream>
+#include <math.h>
+#include <limits>
 using namespace std;
 typedef long long int ll;
+ll hanoi4[1000]={0};
 ll f(ll n){
-	if(!n) return 0;
-	if(n==1) return 1;
-	ll min=1e15;
+	if(hanoi4[n]) return hanoi4[n];
+	if(n==1) return hanoi4[n]=1;
+	ll min=numeric_limits<ll>::max();
 	for(ll i=1; i<n; i++){
-		ll k=2*f(i)+(1<<n-i)-1;
+		ll k=2*f(i)+pow(2, n-i)-1;
+		if(k<0) continue;
 		if(k<min) min=k;
 	}
-	return min;
+	return hanoi4[n]=min;
 }
 int main(){
 	ll n;
