9de26162b66c8e46079ba0a67a5cac40eb5b6960
gshs23078 <129289574+gshs23078@users.noreply.github.com>
2024-03-20 21:59:29 +0900

    Create I.cpp

---
+++ I.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <math.h>
+#include <limits>
+using namespace std;
+typedef long long int ll;
+ll hanoi4[1000]={0};
+ll f(ll n){
+	if(hanoi4[n]) return hanoi4[n];
+	if(n==1) return hanoi4[n]=1;
+	ll min=numeric_limits<ll>::max();
+	for(ll i=1; i<n; i++){
+		ll k=2*f(i)+pow(2, n-i)-1;
+		if(k<0) continue;
+		if(k<min) min=k;
+	}
+	return hanoi4[n]=min;
+}
+int main(){
+	ll n;
+	cin >> n;
+	cout << f(n);
+}
+//E와 동일
